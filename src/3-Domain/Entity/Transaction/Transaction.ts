8f2140543bc223e6d91997c43d30d45d59a8bd6b
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-03-30 10:21:16 -0300

    Refactor Transaction

--- src/3-Domain/Entity/Transaction/Transaction.ts
+++ src/3-Domain/Entity/Transaction/Transaction.ts
@@ -13,32 +13,22 @@ export class Transaction {
     message: string;
 
     isValid() {
-        if (!this.numberRequest) {
-            throw new Error('Campo numberRequest é obrigatório');
-        }
-        if (!this.tid) {
-            throw new Error('Campo tid é obrigatório');
-        }
-        if (!this.kind) {
-            throw new Error('Campo kind é obrigatório');
-        }
-        if (!this.authorizationCode) {
-            throw new Error('Campo authorizationCode é obrigatório');
-        }
-        if (!this.nsu) {
-            throw new Error('Campo nsu é obrigatório');
-        }
-        if (!this.status) {
-            throw new Error('Campo status é obrigatório');
-        }
-        if (!this.amount) {
-            throw new Error('Campo amount é obrigatório');
-        }
-        if (!this.installments) {
-            throw new Error('Campo installments é obrigatório');
-        }
-        if (!this.message) {
-            throw new Error('Campo message é obrigatório');
-        }
+        if (this.numberRequest === undefined) throw new Error('Campo numberRequest é obrigatório');
+
+        if (this.tid === undefined) throw new Error('Campo tid é obrigatório');
+
+        if (this.kind === undefined) throw new Error('Campo kind é obrigatório');
+
+        if (this.authorizationCode === undefined) throw new Error('Campo authorizationCode é obrigatório');
+
+        if (this.nsu === undefined) throw new Error('Campo nsu é obrigatório');
+
+        if (this.status === undefined) throw new Error('Campo status é obrigatório');
+
+        if (this.amount === undefined) throw new Error('Campo amount é obrigatório');
+
+        if (this.installments === undefined) throw new Error('Campo installments é obrigatório');
+
+        if (this.message === undefined) throw new Error('Campo message é obrigatório');
     }
 }
