298365a05d0039811839801093f519815b2fb980
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-04-12 14:05:56 -0300

    Create test TransactionOrder

--- src/Domain/Entity/Transaction/ValueObject/test/TransactionOrder.spec.ts
+++ src/Domain/Entity/Transaction/ValueObject/test/TransactionOrder.spec.ts
@@ -1,5 +1,47 @@
+import { StatusTransaction } from '../../../../../Shared/Enum/StatusTransaction';
+import { TypeTransaction } from '../../../../../Shared/Enum/TypeTransaction.enum';
+import { TransactionOrder } from '../TransactionOrder';
+
+type SutTypes = {
+    numberRequest: string;
+    tid: string;
+    kind: TypeTransaction;
+    authorizationCode: string;
+    nsu: string;
+    status: StatusTransaction;
+    amount: number;
+    installments: number;
+    message: string;
+};
+
+const makeSut = (): SutTypes => {
+    const numberRequest = '100';
+    const tid = '100';
+    const kind = TypeTransaction.CREDIT;
+    const authorizationCode = '100';
+    const nsu = '100';
+    const status = StatusTransaction.NO_CAPTURE;
+    const amount = 100;
+    const installments = 2;
+    const message = 'Teste';
+    return { numberRequest, tid, kind, authorizationCode, nsu, status, amount, installments, message };
+};
+
 describe('TransactionOrder', () => {
-    test('Teste', () => {
-        expect(1).toBe(1);
+    test('Should return not error if correct param', () => {
+        let { numberRequest, tid, kind, authorizationCode, nsu, status, amount, installments, message } = makeSut();
+        const transaction = TransactionOrder.create(
+            numberRequest,
+            tid,
+            kind,
+            authorizationCode,
+            nsu,
+            status,
+            amount,
+            installments,
+            message,
+        );
+        expect(transaction).toBeTruthy();
+        expect(transaction).toBeInstanceOf(TransactionOrder);
     });
 });
