1f8e40b76aef52375b9ce231ca7b86392f8b062b
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-04-12 14:16:47 -0300

    Create test integration TransactionSend

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -1,6 +1,12 @@
-describe('Teste de Integração', () => {
-    test('SendTransaction', () => {
-        // const returnAPI = APP.start(1, 1, false);
-        //console.log(returnAPI);
+import { APP } from './app';
+import { TransactionOrder } from './Domain/Entity/Transaction/ValueObject/TransactionOrder';
+
+describe('TransactionSend', () => {
+    test('Should return not error if to send transactionSend', async () => {
+        const methodUses = 1; //1-Send 2-Search 3-Capture 4-Cancel
+        let gatewayUses = 1; //1-Rede 2- Cielo
+        const returnSendTransaction = await APP.start(methodUses, gatewayUses, false);
+        expect(returnSendTransaction).toBeTruthy();
+        expect(returnSendTransaction).toBeInstanceOf(TransactionOrder);
     });
 });
