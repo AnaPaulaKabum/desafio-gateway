61328dff68e2c2e8981ea03cdb9f600f689d7525
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-04-12 17:51:26 -0300

    Create test get param correct -CaptureOrder

--- src/Domain/Entity/Transaction/ValueObject/test/CaptureOrder.spec.ts
+++ src/Domain/Entity/Transaction/ValueObject/test/CaptureOrder.spec.ts
@@ -23,4 +23,14 @@ describe('CaptureOrder', () => {
         expect(transaction).toBeTruthy();
         expect(transaction).toBeInstanceOf(CaptureOrder);
     });
+
+    test('Should get param correct', () => {
+        let { numberRequest, amount, date, nsu } = makeSut();
+        const transaction = CaptureOrder.create(numberRequest, amount, date, nsu);
+        expect(transaction).toBeTruthy();
+        expect(transaction.numberRequest).toBe(numberRequest);
+        expect(transaction.amount).toBe(amount);
+        expect(transaction.date).toBe(date);
+        expect(transaction.nsu).toBe(nsu);
+    });
 });
