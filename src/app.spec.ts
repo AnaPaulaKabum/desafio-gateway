15e0a04e2fd22aa24d77e1b71d63a5f000cdccae
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-04-12 17:34:26 -0300

    Refactor app.spect.ts

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -3,11 +3,14 @@ import { SearchTransactionOrder } from './Domain/Entity/Transaction/SearchTransa
 import { Card } from './Domain/Entity/Transaction/ValueObject/Card';
 import { TransactionOrder } from './Domain/Entity/Transaction/ValueObject/TransactionOrder';
 
+const gatewayUses = 1; //1-Rede 2- Cielo
+
 describe('TransactionSend', () => {
     test('Should return not error if to send transactionSend', async () => {
         const methodUses = 1; //1-Send 2-Search 3-Capture 4-Cancel
-        let gatewayUses = 1; //1-Rede 2- Cielo
+
         const returnSendTransaction = await APP.start(methodUses, gatewayUses, false);
+
         expect(returnSendTransaction).toBeTruthy();
         expect(returnSendTransaction).toBeInstanceOf(TransactionOrder);
     });
@@ -15,13 +18,13 @@ describe('TransactionSend', () => {
 
 describe('SearchTransaction', () => {
     test('Should return not error if to send searchTransaction', async () => {
-        const methodUses = 1; //1-Send 2-Search 3-Capture 4-Cancel
-        let gatewayUses = 2; //1-Rede 2- Cielo
+        const methodUses = 2; //1-Send 2-Search 3-Capture 4-Cancel
+
         const returnSearchTransaction = await APP.start(methodUses, gatewayUses, false);
 
         expect(returnSearchTransaction).toBeTruthy();
-        expect(returnSearchTransaction).toBeInstanceOf(SearchTransactionOrder);
         expect(returnSearchTransaction.card).toBeInstanceOf(Card);
         expect(returnSearchTransaction.transaction).toBeInstanceOf(TransactionOrder);
+        expect(returnSearchTransaction).toBeInstanceOf(SearchTransactionOrder);
     });
 });
