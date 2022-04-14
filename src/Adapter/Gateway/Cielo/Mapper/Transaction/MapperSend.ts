41f8ed518c8ccb60099c00d3452c67211bf5e89d
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-04-14 10:36:32 -0300

    Refactor MapperSendCielo

--- src/Adapter/Gateway/Cielo/Mapper/Transaction/MapperSend.ts
+++ src/Adapter/Gateway/Cielo/Mapper/Transaction/MapperSend.ts
@@ -4,32 +4,42 @@ import { TypeTransaction } from '../../../../../Shared/Enum/TypeTransaction.enum
 import { TransactionOrder } from '../../../../../Domain/Entity/Transaction/ValueObject/TransactionOrder';
 import { SendCreditCieloTransitionResponse } from '../../Response/SendCreditCieloTransitionResponse';
 import { SendDebitTransitionResponse } from '../../Response/SendDebitTransitionResponse';
+import { json } from 'stream/consumers';
 
-export abstract class MapperSend {
-    static toTransaction(Json: any, typeTransaction: TypeTransaction): TransactionOrder {
+export class MapperSend {
+    public static toTransaction(Json: any, typeTransaction: TypeTransaction): TransactionOrder {
         if (typeTransaction === TypeTransaction.CREDIT) {
-            let object = plainToInstance(SendCreditCieloTransitionResponse, Json);
-
-            const numberRequest = object.Payment.PaymentId;
-            const tid = object.Payment.Tid;
-            const authorizationCode = object.Payment.AuthorizationCode;
-            const nsu = object.Payment.ProofOfSale;
-            const amount = object.Payment.Amount;
-            const installments = object.Payment.Installments;
-            const message = object.Payment.ReturnMessage;
-
-            return TransactionOrder.create(
-                numberRequest,
-                tid,
-                typeTransaction,
-                authorizationCode,
-                nsu,
-                StatusTransaction.NO_CAPTURE,
-                amount,
-                installments,
-                message,
-            );
+            return MapperSend.transactionCredit(json);
         }
+
+        return MapperSend.transactionDebit(json);
+    }
+
+    private static transactionCredit(Json: any) {
+        let object = plainToInstance(SendCreditCieloTransitionResponse, Json);
+
+        const numberRequest = object.Payment.PaymentId;
+        const tid = object.Payment.Tid;
+        const authorizationCode = object.Payment.AuthorizationCode;
+        const nsu = object.Payment.ProofOfSale;
+        const amount = object.Payment.Amount;
+        const installments = object.Payment.Installments;
+        const message = object.Payment.ReturnMessage;
+
+        return TransactionOrder.create(
+            numberRequest,
+            tid,
+            TypeTransaction.CREDIT,
+            authorizationCode,
+            nsu,
+            StatusTransaction.NO_CAPTURE,
+            amount,
+            installments,
+            message,
+        );
+    }
+
+    private static transactionDebit(Json: any) {
         let object = plainToInstance(SendDebitTransitionResponse, Json);
         const numberRequest = object.Payment.PaymentId;
         const tid = object.Payment.Tid;
@@ -42,7 +52,7 @@ export abstract class MapperSend {
         return TransactionOrder.create(
             numberRequest,
             tid,
-            typeTransaction,
+            TypeTransaction.DEBIT,
             authorizationCode,
             nsu,
             StatusTransaction.NO_CAPTURE,
