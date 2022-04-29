9c46e8f0c3d49451dcb0bc4956fb40698c217209
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-04-29 09:59:15 -0300

    Refactor SearchTransactionOrder

--- src/Domain/Entity/Transaction/SearchTransactionOrder.ts
+++ src/Domain/Entity/Transaction/SearchTransactionOrder.ts
@@ -1,68 +1,38 @@
 import { TransactionOrder } from './TransactionOrder';
 import { SearchTransactionOrderDTO } from '../../../Shared/DTO/Order/SearchTransactionOrder';
-import { TransactionOrderDTO } from '../../../Shared/DTO/Order/TransactionOrderDTO';
 
 export class SearchTransactionOrder {
-    private constructor(
+    constructor(
         private readonly _transaction: TransactionOrder,
         private readonly _captureAmount: number,
         private readonly _captureDate: Date,
         private readonly _cancelAmount: number,
         private readonly _cancelDate: Date,
         private readonly _numberCreditCard: string,
-    ) {}
+    ) {
+        if (!_numberCreditCard) throw new Error('Campo numberCreditCard é obrigatório');
+
+        if (_captureAmount > 0) {
+            if (!_captureAmount) throw new Error('Campo captureAmount é obrigatório');
+            if (!_captureDate) throw new Error('Campo captureDate é obrigatório');
+        }
+
+        if (_cancelAmount > 0) {
+            if (!_cancelAmount) throw new Error('Campo cancelAmount é obrigatório');
+            if (!_cancelDate) throw new Error('Campo cancelDate é obrigatório');
+        }
+    }
 
     static createForDTO(searchTransactionDTO: SearchTransactionOrderDTO): SearchTransactionOrder {
-        return SearchTransactionOrder.create(
-            searchTransactionDTO.transaction,
+        const transactionOrder = TransactionOrder.createForDTO(searchTransactionDTO.transaction);
+
+        return new SearchTransactionOrder(
+            transactionOrder,
             searchTransactionDTO.captureAmount,
             searchTransactionDTO.captureDate,
             searchTransactionDTO.cancelAmount,
             searchTransactionDTO.cancelDate,
             searchTransactionDTO.numberCreditCard,
         );
-    }
-
-    static create(
-        transactionDTO: TransactionOrderDTO,
-        captureAmount: number,
-        captureDate: Date,
-        cancelAmount: number,
-        cancelDate: Date,
-        numberCreditCard: string,
-    ) {
-        const transaction = new TransactionOrder(
-            transactionDTO.numberRequest,
-            transactionDTO.tid,
-            transactionDTO.kind,
-            transactionDTO.status,
-            transactionDTO.amount,
-            transactionDTO.message,
-            transactionDTO.nsu,
-            transactionDTO.authorizationCode,
-            transactionDTO.installments,
-        );
-
-        if (!numberCreditCard) throw new Error('Campo numberCreditCard é obrigatório');
-
-        if (captureAmount > 0) {
-            if (!captureAmount) throw new Error('Campo captureAmount é obrigatório');
-            if (!captureDate) throw new Error('Campo captureDate é obrigatório');
-        }
-
-        if (cancelAmount > 0) {
-            if (!cancelAmount) throw new Error('Campo cancelAmount é obrigatório');
-            if (!cancelDate) throw new Error('Campo cancelDate é obrigatório');
-        }
-        const searchTransaction = new SearchTransactionOrder(
-            transaction,
-            captureAmount,
-            captureDate,
-            cancelAmount,
-            cancelDate,
-            numberCreditCard,
-        );
-
-        return searchTransaction;
     }
 }
--- src/test/Domain/Entity/Transaction/SearchTransactionOrder.spec.ts
+++ src/test/Domain/Entity/Transaction/SearchTransactionOrder.spec.ts
@@ -1,5 +1,6 @@
 import { CancelOrder } from '../../../../Domain/Entity/Transaction/CancelOrder';
 import { CaptureOrder } from '../../../../Domain/Entity/Transaction/CaptureOrder';
+import { SearchTransactionOrder } from '../../../../Domain/Entity/Transaction/SearchTransactionOrder';
 import { TransactionOrder } from '../../../../Domain/Entity/Transaction/TransactionOrder';
 import { StatusTransaction } from '../../../../Shared/Enum/StatusTransaction';
 import { TypeTransaction } from '../../../../Shared/Enum/TypeTransaction.enum';
