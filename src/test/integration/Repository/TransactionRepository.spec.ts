e6a23aad348aa8525cd51719a55c83808a850e7e
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-05-23 14:30:21 -0300

    refactor teste transactionRepository

--- src/test/integration/Repository/TransactionRepository.spec.ts
+++ src/test/integration/Repository/TransactionRepository.spec.ts
@@ -1,31 +1,45 @@
 import { TransactionOrder } from '../../../Domain/Entity/Transaction/TransactionOrder';
 import { ConnectDBTypeORM } from '../../../Infra/ConnectBD/TypeORM/ConnectDBTypeORM';
+import { CancelOrderEntity } from '../../../Infra/ConnectBD/TypeORM/Entity/CancelOrderEntity';
+import { CaptureOrderEntity } from '../../../Infra/ConnectBD/TypeORM/Entity/CaptureOrderEntity';
 import { TransactionOrderEntity } from '../../../Infra/ConnectBD/TypeORM/Entity/TransactionOrderEntity';
 import { TransactionRepository } from '../../../Infra/ConnectBD/TypeORM/Repository/Transaction/TransactionRepository';
 import { StatusTransaction } from '../../../Shared/Enum/StatusTransaction';
 import { TypeTransaction } from '../../../Shared/Enum/TypeTransaction.enum';
 
-const connect = new ConnectDBTypeORM(TransactionOrderEntity);
+let connect: ConnectDBTypeORM;
+let repository: TransactionRepository;
 
 describe('Repository : TransactionRepository', () => {
-    const repositoryTransaction = new TransactionRepository(connect.appDataSource.manager);
-    test('SaveTransaction', async () => {
+    beforeEach(async function () {
+        connect = new ConnectDBTypeORM([TransactionOrderEntity, CaptureOrderEntity, CancelOrderEntity]);
         await connect.start();
-        const transactionOrder = new TransactionOrder(
-            'pedido123',
-            '100',
-            TypeTransaction.CREDIT,
-            StatusTransaction.NO_CAPTURE,
-            100,
-            'Teste',
-            '100',
-            '100',
-            1,
-        );
-        const result = await repositoryTransaction.saveTransaction(transactionOrder);
+        repository = new TransactionRepository(connect.appDataSource.manager);
+    });
 
-        expect(result).toBeTruthy();
+    afterEach(async function () {
+        await connect.close();
+    });
 
-        connect.close();
+    describe('SaveTransaction', () => {
+        test('Não deve retornar error', async () => {
+            connect.appDataSource.options;
+
+            repository = new TransactionRepository(connect.appDataSource.manager);
+            const transactionOrder = new TransactionOrder(
+                'pedido123',
+                '100',
+                TypeTransaction.CREDIT,
+                StatusTransaction.NO_CAPTURE,
+                100,
+                'Teste',
+                '100',
+                '100',
+                1,
+            );
+            const result = await repository.saveTransaction(transactionOrder);
+
+            expect(result).toBeTruthy();
+        });
     });
 });
