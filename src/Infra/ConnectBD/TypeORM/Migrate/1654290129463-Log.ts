073c8ccb75c72fb5d78e883e89bcc773b749728b
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-06-06 10:23:59 -0300

    upt: correct Log migrate

--- src/Infra/ConnectBD/TypeORM/Migrate/1654290129463-Log.ts
+++ src/Infra/ConnectBD/TypeORM/Migrate/1654290129463-Log.ts
@@ -3,10 +3,10 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class Log1654290129463 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE Log (
-            user INT NULL,
+            id VARCHAR(250),
+            userCode INT NULL,
             descripto varchar(255) NULL,
             process INT NULL,
-            gravity INT NULL,
             date Date
         )`);
     }
