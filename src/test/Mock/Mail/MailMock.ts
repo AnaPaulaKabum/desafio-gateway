0d43947d4b2d49f8394255cb08e27aded641428e
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-06-09 10:50:12 -0300

    Modificado MailMock para retornar uma promisses

--- src/test/Mock/Mail/MailMock.ts
+++ src/test/Mock/Mail/MailMock.ts
@@ -3,6 +3,8 @@ import { IMail } from '../../../Shared/Interfaces/Mail/IMail';
 
 export class MailMock implements IMail {
     send(fieldMail: FieldMail): Promise<any> {
-        throw new Error('Method not implemented Mail - send');
+        return new Promise(function (resolve) {
+            resolve(null);
+        });
     }
 }
