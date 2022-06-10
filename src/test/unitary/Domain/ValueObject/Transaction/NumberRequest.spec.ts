aa52252e261635b82fa02172de9436cb50331589
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-06-10 10:22:54 -0300

    Create test NumberRequest

---
+++ src/test/unitary/Domain/ValueObject/Transaction/NumberRequest.spec.ts
@@ -0,0 +1,15 @@
+import { NumberRequest } from '../../../../../Domain/Entity/Transaction/ValueObject/Transaction/NumberRequest';
+
+describe('NumberRequest- isValidSend', () => {
+    test('Should return error if numberRequest > max_length', () => {
+        expect(() => new NumberRequest('1234567890123456789')).toThrow();
+    });
+    test('Should return error if numberRequest is null', () => {
+        expect(() => new NumberRequest('')).toThrow();
+    });
+
+    test('Should return object NumberRequest if numberRequest < max_length', () => {
+        const numberRequest = new Number('123456');
+        expect(numberRequest).toBeTruthy();
+    });
+});
