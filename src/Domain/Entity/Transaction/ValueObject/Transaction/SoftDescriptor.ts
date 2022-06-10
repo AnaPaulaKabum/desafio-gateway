dd6db9c1713a7889169584e48672695e9ba89452
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-06-10 14:37:35 -0300

    upt: method valid sofDescriptor is null

--- src/Domain/Entity/Transaction/ValueObject/Transaction/SoftDescriptor.ts
+++ src/Domain/Entity/Transaction/ValueObject/Transaction/SoftDescriptor.ts
@@ -1,6 +1,7 @@
 export class SoftDescriptor {
     private softDescriptor_MAX = 18;
     constructor(private _value: string) {
+        if (!_value) throw new Error('SoftDescriptor é obrigatório');
         if (_value.length > this.softDescriptor_MAX) {
             throw new Error('SoftDescriptor deverá ter menos ' + this.softDescriptor_MAX + ' caracter');
         }
