7e3b1d91d5e071ff8a40fd2bc557fecddef5917a
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-06-10 14:35:42 -0300

    test:Created ValueObject SoftDescriptor

---
+++ src/test/unitary/Domain/ValueObject/Transaction/SoftDescriptor.spec.ts
@@ -0,0 +1,16 @@
+import { SoftDescriptor } from '../../../../../Domain/Entity/Transaction/ValueObject/Transaction/SoftDescriptor';
+
+describe('SoftDescriptor', () => {
+    test('Should return error if softDescriptor invalid', () => {
+        expect(() => new SoftDescriptor('testetestetestetestetesteteste')).toThrow();
+    });
+
+    test('Should return error if softDescriptor is null', () => {
+        expect(() => new SoftDescriptor('')).toThrow();
+    });
+
+    test('Should return object softDescriptor param valid', () => {
+        const numberRequest = new SoftDescriptor('Compra_VALID');
+        expect(numberRequest).toBeTruthy();
+    });
+});
