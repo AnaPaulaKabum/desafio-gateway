ca1eccb034aa5bc8de76dbdbeeb7ae93220b1436
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-06-10 14:15:42 -0300

    Created ValueObject Amount

---
+++ src/Domain/Entity/Transaction/ValueObject/Transaction/Amount.ts
@@ -0,0 +1,10 @@
+export class Amount {
+    private amount_MAX = 10;
+
+    constructor(private _value: string) {
+        if (!_value) throw new Error('Amount deverá ser informado');
+        if (_value.replace('.', '').length > this.amount_MAX) {
+            throw new Error('Amount deverá ter menos ' + this.amount_MAX + ' caracteres');
+        }
+    }
+}
