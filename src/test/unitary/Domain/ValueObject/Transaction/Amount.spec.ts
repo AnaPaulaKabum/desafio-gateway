9f3a0b44791b939f457c2ebc04c5fa31b6cfce09
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-06-10 14:15:59 -0300

    test:Created ValueObject Amount

---
+++ src/test/unitary/Domain/ValueObject/Transaction/Amount.spec.ts
@@ -0,0 +1,20 @@
+import { Amount } from '../../../../../Domain/Entity/Transaction/ValueObject/Transaction/Amount';
+
+describe('Amount', () => {
+    test('Should return error if amount invalid', () => {
+        expect(() => {
+            new Amount('100000000000');
+        }).toThrow();
+    });
+
+    test('Should return error if amount invalid', () => {
+        expect(() => {
+            new Amount('');
+        }).toThrow();
+    });
+
+    test('Should return object Amount with param valid.', () => {
+        const amount = new Amount('150.00');
+        expect(amount).toBeTruthy();
+    });
+});
