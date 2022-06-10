8f2e77756b560bebad6c556ebfa4683ec9d83cba
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-06-10 10:44:39 -0300

    test:Created ObjectValue Installments

---
+++ src/test/unitary/Domain/ValueObject/Transaction/Installments.spec.ts
@@ -0,0 +1,21 @@
+import { Installments } from '../../../../../Domain/Entity/Transaction/ValueObject/Transaction/Installments';
+
+describe('Installments', () => {
+    test('Should return error if installments in < 0', () => {
+        expect(() => {
+            new Installments(-5);
+        }).toThrow();
+    });
+
+    test('Should return error if installments invalid ', () => {
+        expect(() => {
+            new Installments(24);
+        }).toThrow();
+    });
+
+    test('Should object installments if installments correct ', () => {
+        const installments = new Installments(5);
+
+        expect(installments).toBeTruthy();
+    });
+});
